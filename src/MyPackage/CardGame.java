705983132a585c372451b83fc4fe9e65b91d7945
touhidpasha <touhidpasha552@gmail.com>
2021-09-08 19:00:42 +0530

    distributed-for each player

--- src/MyPackage/CardGame.java
+++ src/MyPackage/CardGame.java
@@ -13,6 +13,7 @@ public class CardGame {
 	public String[] rank;
 	public String[] suit;
 	public Set<String> cards = new HashSet<String>();
+	public String players[][];
 		
 	
 
@@ -29,18 +30,31 @@ public class CardGame {
 		    {
 		        int row=((int) ((Math.random() * (5 - 1)) + 1));
 		        int column=((int) ((Math.random() * (14 - 1)) + 1));
-		       
 		        cards.add(String.valueOf(row+"+"+column));
 		    }
 		    //System.out.print(cards);
 		}
 	
   
+	public void distributeCards(int numberOfPlayers) {
+		players=new String[numberOfPlayers][13];//here each row will contain players and column will contain cards for each of one
+		String[] cardsArray = cards.toArray(new String[cards.size()]);//converting set to array
+		int k=0;
+		for(int i=0;i<(numberOfPlayers);i++)
+		for(int j=0;j<(13);j++)
+		{
+			
+			players[i][j]=cardsArray[k];
+		}
+			}
 
-	public void printCards() {
+	/*public void printCards() {
 		System.out.print(cards.size());
 		System.out.print(cards);
-	}
+		for(int i=0;i<cardsArray.length;i++)
+		System.out.println(cardsArray[i]);
+
+	}*/
 	
 	
 	
--- src/MyPackage/MainClass.java
+++ src/MyPackage/MainClass.java
@@ -4,8 +4,8 @@ public class MainClass {
 public static void main(String args[]) {
 	CardGame game=new CardGame();
 	Players players=new Players(4);//setting players count 4
-		//game.printCards();
+		
 		game.shuffle(players.numberOfPlayers);
-		game.printCards();
+		game.distributeCards(players.numberOfPlayers);
 	}
 }
